788057992f9b0018a0604bca9a03efd5bb549021
Dilip492 <dpal99113@gmail.com>
2024-06-10 20:06:42 +0530

    first commit

---
+++ second.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a;
+
+    cout << "enter the value of a " << endl;
+
+    cin >> a;
+    cout << "this is the value of a " << a << endl;
+
+    return 0;
+}
